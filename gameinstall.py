73a12239b8e8dd52231b085b133381631b9bb497
littleumbrellas <littleumbrellas@proton.me>
2024-08-06 01:19:45 -0400

    B)

--- gameinstall.py
+++ gameinstall.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-from pathlib import Path
 import fnmatch
 import os
 import sys
 
-def find_file(file_pattern: str, matches: list[str], root_path=".") -> bool:
-    foundFile = False
-    for root, _, files in os.walk(root_path):
+def find_files(file_pattern: str, matches: list[str], path: str = ".") -> None:
+    for root, _, files in os.walk(path):
         for file in files:
             if fnmatch.fnmatch(file, file_pattern):
-                if not foundFile:
-                    foundFile = True
                 matches.append(os.path.join(root, file))
-    return foundFile
 
 def choose_file(files: list[str]) -> str:
     while True:
@@ -35,26 +30,31 @@ def choose_file(files: list[str]) -> str:
             continue
         return files[choice-1]
 
-HOME = os.getenv("HOME")
+HOME      = os.getenv("HOME")
 assert(HOME)
 
 DOWNLOADS = os.path.join(HOME, "Downloads")
 GAMES     = os.path.join(HOME, ".wine", "drive_c", "Games")
 DESKTOP   = os.path.join(HOME, "Desktop")
-assert(os.path.exists(DOWNLOADS))
-assert(os.path.exists(GAMES))
-assert(os.path.exists(DESKTOP))
+
+assert(os.path.isdir(DOWNLOADS))
+assert(os.path.isdir(GAMES))
+assert(os.path.isdir(DESKTOP))
 
 os.chdir(DOWNLOADS)
 
+# -- IN DOWNLOADS -- #
+
 setup_files = []
-if not find_file("setup*.exe", setup_files):
+find_files("setup*.exe", setup_files)
+if not setup_files:
     print("\nERROR: No setup files were found", file=sys.stderr)
     sys.exit(1)
 
 setup_files.sort(key=os.path.getmtime, reverse=True)
-confirm = input(f"Is '{setup_files[0]}' the game you want to install? [Y/n]: ")
+latest_file = setup_files[0]
 
+confirm = input(f"Is '{latest_file}' the game you want to install? [Y/n]: ")
 if confirm.lower() == "n":
     setup_files.pop(0)
     setup = choose_file(setup_files)
@@ -62,9 +62,11 @@ else:
     setup = setup_files[0]
 
 before_install = os.listdir(GAMES)
+
 ret = os.system("wine '" + setup + "'")
 if not ret:
     print("WARNING: Non-zero exit code returned from wine", file=sys.stderr)
+
 after_install  = os.listdir(GAMES)
 
 new_games = [n for n in after_install if n not in before_install]
@@ -74,10 +76,11 @@ if not new_games:
 
 os.chdir(GAMES)
 
+# -- IN GAMES -- #
+
 exe_files = []
 for n in new_games:
-    if os.path.isdir(n):
-        find_file("*.exe", exe_files, n)
+    find_files("*.exe", exe_files, n)
 
 if not exe_files:
     print("\nERROR: No exe files were found", file=sys.stderr)
@@ -88,12 +91,20 @@ if len(exe_files) > 1:
 else:
     exe = exe_files[0]
 
-game_name     = str(Path(exe).parent)
-game_path     = os.path.join(GAMES, game_name)
-launcher_file = os.path.join(DESKTOP, game_name + ".command")
+parts    = os.path.normpath(exe).split('/')
+name     = parts[0]
+exe      = os.path.join(*parts[1:])
+path     = os.path.join(GAMES, name)
+shortcut = os.path.join(DESKTOP, (name + ".command"))
 
-fd = os.open(launcher_file, os.O_WRONLY|os.O_CREAT, mode=0o755)
+if os.path.isfile(shortcut):
+    confirm = input(f"\nOverwrite existing file '{shortcut}'? [y/N]: ")
+    if confirm.lower() != 'y' and confirm.lower() != "yes":
+        sys.exit()
+    os.remove(shortcut)
+
+fd = os.open(shortcut, os.O_WRONLY|os.O_CREAT, mode=0o755)
 with open(fd, 'w') as f:
     f.write("#!/bin/sh\n")
-    f.write("cd '" + game_path + "'\n")
-    f.write("wine '" + exe.replace(game_name + os.sep, "") + "'")
+    f.write(f"cd '{path}'\n")
+    f.write(f"wine '{exe}'\n")
